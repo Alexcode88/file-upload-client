f8cee5d97971c41cec6a037371abf834738c865e
Alfredo08 <alfredosv08@gmail.com>
2021-12-06 14:16:24 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'file-upload-client';
+  image: any = "";
+
+  constructor( private _http: HttpClient ){
+
+  }
+
+  selectImage( event: any ): void{
+    if( event.target.files.length > 0 ){
+      const file:any = event.target.files[0];
+      this.image = file;
+    }
+  }
+
+  submitImage( event: any ): void{
+    event.preventDefault();
+    const formData: FormData = new FormData();
+    formData.append( 'myImage', this.image );
+
+    this._http.post<any>( 'http://localhost:8080/upload', formData )
+      .subscribe( (data: any) => {
+        console.log( data );
+      });
+  }
 }
+
